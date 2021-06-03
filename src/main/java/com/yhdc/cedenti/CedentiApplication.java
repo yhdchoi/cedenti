24b679dbd029336006ea876ef182778208322fe6
yhdchoi <yhdchoi@gmail.com>
2021-06-04 00:06:16 +0900

    Initial commit

---
+++ src/main/java/com/yhdc/cedenti/CedentiApplication.java
@@ -0,0 +1,13 @@
+package com.yhdc.cedenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedentiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhdc/cedenti/CedentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhdc.cedenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
