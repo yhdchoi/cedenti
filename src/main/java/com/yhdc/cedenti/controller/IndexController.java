a1bb1650002e9d043864270865d52a2f5fdba626
yhdchoi <yhdchoi@gmail.com>
2021-06-12 23:00:48 +0900

    Frontend
    Pages added.

--- src/main/java/com/yhdc/cedenti/controller/IndexController.java
+++ src/main/java/com/yhdc/cedenti/controller/IndexController.java
@@ -12,5 +12,34 @@ public class IndexController {
 	public String index() {
 		return "home/index";
 	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
+		
+	@GetMapping("/board/browse")
+	public String browse() {
+		return "board/browse";
+	}
+	
+	@GetMapping("/user/dashboard")
+	public String dashboard() {
+		return "user/dashboard";
+	}
 
+	@GetMapping("/user/profile")
+	public String profile() {
+		return "user/profile";
+	}
+	
+	@GetMapping("/user/setting")
+	public String setting() {
+		return "user/setting";
+	}
 }
